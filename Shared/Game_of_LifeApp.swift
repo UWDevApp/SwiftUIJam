4485d67aa42aa657381c00016e3e487fa52be2e9
ApolloZhu <public-apollonian@outlook.com>
2021-02-20 00:00:04 -0500

    Setup project

---
+++ Shared/Game_of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_of_LifeApp.swift
+//  Shared
+//
+//  Created by Apollo Zhu on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
